3b72e00dc326bee9c88e4331275cd52f0a021c85
Yutaka Shimada <shimayu22@gmail.com>
2020-07-13 13:38:21 +0900

    todoリスト作成

---
+++ todo/pages/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        todo
+      </h1>
+      <input type="text" v-model="newItem">
+      <form @submit.prevent>
+        <button @click="addItem">add</button>
+      </form>
+      <li v-for="todo in todos">
+        {{ todo.item }}
+      </li>
+      <h2 class="subtitle">
+        リスト
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  data() {
+    return {
+      newItem: '',
+      todos: []
+    }
+  },
+  methods: {
+    addItem: function(event) {
+      var todo = {
+        item: this.newItem
+      };
+      this.todos.push(todo)
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
